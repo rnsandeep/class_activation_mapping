4120402f425ac511bf46e3a9a5d612cddc43a833
Naga Sandeep Ramachandruni <sandeep@heallo.ai>
2019-07-11 16:37:54 +0530

    changes to torch vision inception model to return cam  and flatten output

--- inception.py
+++ inception.py
@@ -116,20 +116,20 @@ class Inception3(nn.Module):
         # N x 1280 x 8 x 8
         x = self.Mixed_7b(x)
         # N x 2048 x 8 x 8
-        x = self.Mixed_7c(x)
+        cam = self.Mixed_7c(x)
         # N x 2048 x 8 x 8
         # Adaptive average pooling
-        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = F.adaptive_avg_pool2d(cam, (1, 1))
         # N x 2048 x 1 x 1
         x = F.dropout(x, training=self.training)
         # N x 2048 x 1 x 1
-        x = x.view(x.size(0), -1)
+        flatten = x.view(x.size(0), -1)
         # N x 2048
         x = self.fc(flatten)
         # N x 1000 (num_classes)
         if self.training and self.aux_logits:
             return x, aux
-        return x
+        return x, cam, flatten
 
 
 class InceptionA(nn.Module):
